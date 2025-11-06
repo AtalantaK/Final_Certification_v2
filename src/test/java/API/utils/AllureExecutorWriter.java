86637c2b48af2c4646ad17769dae27bc765eb356
Kseniia Kalashnikova <kseniia.kalashnikova@netcracker.com>
2025-11-06 19:49:58 +0500

    Github Workflow refactoring, Allure refactoring

--- src/test/java/API/utils/AllureExecutorWriter.java
+++ src/test/java/API/utils/AllureExecutorWriter.java
@@ -7,15 +7,22 @@ import java.io.IOException;
 
 public class AllureExecutorWriter {
     public static void createExecutorFile() {
+
+        String repo = System.getenv("GITHUB_REPOSITORY");
+        String runId = System.getenv("GITHUB_RUN_ID");
+        String runNumber = System.getenv("GITHUB_RUN_NUMBER");
+        String serverUrl = System.getenv("GITHUB_SERVER_URL");
+        String buildStatus = System.getenv("BUILD_STATUS"); // берём из GITHUB_ENV - maven.yml
+
         JSONObject executor = new JSONObject();
         executor.put("name", "GitHub Actions");
         executor.put("type", "github");
-        executor.put("url", "https://github.com/AtalantaK/Final_Certification_v2/actions");
-//        executor.put("buildOrder", 101);
-//        executor.put("buildName", "CI Build #101");
-//        executor.put("buildUrl", "https://github.com/AtalantaK/Final_Certification_v2/actions/runs/101");
+        executor.put("url", serverUrl + "/" + repo + "/actions");
+        executor.put("buildOrder", Integer.parseInt(runNumber));
+        executor.put("buildName", "Build #" + runNumber);
+        executor.put("buildUrl", serverUrl + "/" + repo + "/actions/runs/" + runId);
         executor.put("reportUrl", "https://atalantak.github.io/Final_Certification_v2/");
-        executor.put("buildStatus", "SUCCESS");
+        executor.put("buildStatus", buildStatus);
 
         try (FileWriter file = new FileWriter("target/allure-results/executor.json")) {
             file.write(executor.toString(4));
